49f80527837a0368d377df2fba52ac7fc948fdae
enPiT-T20 <v065ff@yamaguchi-u.ac.jp>
2017-08-25 17:11:37 +0900

    途中

--- MyDiary/app/src/main/java/com/example/yu_enpit/mydiary/DiaryListFragment.java
+++ MyDiary/app/src/main/java/com/example/yu_enpit/mydiary/DiaryListFragment.java
@@ -17,6 +17,8 @@ import android.view.ViewGroup;
  * Use the {@link DiaryListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
+
 public class DiaryListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
